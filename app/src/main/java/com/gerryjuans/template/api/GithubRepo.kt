de76ee58f148c3ecf928b992e6f8c505398548ed
Gerry <gerry.juans@traveloka.com>
2020-09-17 22:52:01 +0700

    Update response class

--- app/src/main/java/com/gerryjuans/template/api/GithubRepo.kt
+++ app/src/main/java/com/gerryjuans/template/api/GithubRepo.kt
@@ -3,5 +3,9 @@ package com.gerryjuans.template.api
 data class GithubRepo(
     val name: String?,
     val avatar: String?,
-    val author: String?
+    val author: String?,
+    val description: String?,
+    val language: String?,
+    val stars: Int,
+    val forks: Int
 )
