29e3f2786f051ffc0146ca0832a5b08d07b1c0f1
Gerry <gerry.juans@traveloka.com>
2020-09-19 10:23:11 +0700

    Add unit tests for data loader

---
+++ app/src/test/java/com/gerryjuans/template/trending/usecase/TrendingLoaderTest.kt
@@ -0,0 +1,92 @@
+package com.gerryjuans.template.trending.usecase
+
+import com.gerryjuans.template.trending.TrendingModel
+import com.gerryjuans.template.trending.TrendingProvider
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import io.mockk.verifySequence
+import org.junit.Before
+import org.junit.Test
+import org.threeten.bp.LocalDate
+import org.threeten.bp.LocalDateTime
+import org.threeten.bp.LocalTime
+
+class TrendingLoaderTest {
+
+    @MockK
+    lateinit var callback: TrendingLoader.Callback
+
+    @MockK
+    lateinit var trendingProvider: TrendingProvider
+
+    @MockK
+    lateinit var timeChecker: TrendingTimeChecker
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this, relaxed = true)
+    }
+
+    private val currentTime = LocalDateTime.of(
+        LocalDate.of(2020, 9, 19),
+        LocalTime.of(18, 0)
+    )
+
+    private fun run() =
+        TrendingLoader(trendingProvider, timeChecker).load(callback, currentTime)
+
+    @Test
+    fun `if is loaded then update list only`() {
+        every { callback.isLoaded } returns true
+        run()
+        verifySequence {
+            callback.isLoaded
+            callback.updateListAndScroll()
+        }
+    }
+
+    @Test
+    fun `if not loaded and prev data null should call api`() {
+        every { callback.isLoaded } returns false
+        every { trendingProvider.load() } returns null
+        run()
+        verifySequence {
+            callback.isLoaded
+            trendingProvider.load()
+            callback.populateFromApi()
+        }
+    }
+
+    @Test
+    fun `if not loaded and prev data valid but expired should call api`() {
+        every { callback.isLoaded } returns false
+        every { trendingProvider.load() } returns mockk(relaxed = true)
+        every { timeChecker.isDataExpired(any(), any()) } returns true
+        run()
+        verifySequence {
+            callback.isLoaded
+            trendingProvider.load()
+            timeChecker.isDataExpired(any(), any())
+            callback.populateFromApi()
+        }
+    }
+
+    @Test
+    fun `if not loaded and prev data valid should update model, list, and set load flag to true`() {
+        val prevData = mockk<TrendingModel>(relaxed = true)
+        every { callback.isLoaded } returns false
+        every { trendingProvider.load() } returns prevData
+        every { timeChecker.isDataExpired(any(), any()) } returns false
+        run()
+        verifySequence {
+            callback.isLoaded
+            trendingProvider.load()
+            timeChecker.isDataExpired(any(), any())
+            callback.updateModel(prevData)
+            callback.updateListAndScroll()
+            callback.isLoaded = true
+        }
+    }
+}
