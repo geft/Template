bed73d847b3574cdc2f3174a08c3ac46db1ef96c
Gerry <gerry.juans@traveloka.com>
2020-09-20 22:53:46 +0700

    Remove appComponent from base app

--- app/src/main/java/com/gerryjuans/template/base/BaseApplication.kt
+++ app/src/main/java/com/gerryjuans/template/base/BaseApplication.kt
@@ -2,9 +2,6 @@ package com.gerryjuans.template.base
 
 import android.util.Log
 import androidx.multidex.MultiDexApplication
-import com.gerryjuans.template.di.AppComponent
-import com.gerryjuans.template.di.DaggerAppComponent
-import com.gerryjuans.template.di.ProviderModule
 import com.google.android.gms.security.ProviderInstaller
 import com.jakewharton.threetenabp.AndroidThreeTen
 
@@ -14,15 +11,12 @@ open class BaseApplication : MultiDexApplication() {
         const val BASE_URL = "https://ghapi.huchen.dev/"
     }
 
-    lateinit var appComponent: AppComponent
-
     override fun onCreate() {
         super.onCreate()
 
         setupGooglePlayProvider()
         setupUncaughtExceptionHandling()
         initThreeTen()
-        initAppComponent()
     }
 
     // for Kitkat
@@ -42,12 +36,6 @@ open class BaseApplication : MultiDexApplication() {
 
     private fun initThreeTen() {
         AndroidThreeTen.init(this)
-    }
-
-    private fun initAppComponent() {
-        appComponent = DaggerAppComponent.builder()
-            .providerModule(ProviderModule(this))
-            .build()
     }
 
     open fun getBaseUrl() = BASE_URL
