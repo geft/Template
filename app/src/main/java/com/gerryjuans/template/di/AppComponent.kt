ccad52e756526e9002431139ee26c67c2922b208
Gerry <gerry.juans@traveloka.com>
2020-10-06 20:42:50 +0700

    Remove irrelevant items

--- app/src/main/java/com/gerryjuans/template/di/AppComponent.kt
+++ app/src/main/java/com/gerryjuans/template/di/AppComponent.kt
@@ -1,10 +1,9 @@
 package com.gerryjuans.template.di
 
-import com.gerryjuans.template.trending.TrendingActivity
 import dagger.Component
 
 @Component(modules = [ProviderModule::class])
 interface AppComponent {
 
-    fun inject(trendingActivity: TrendingActivity)
+
 }
--- app/src/main/java/com/gerryjuans/template/trending/TrendingSortType.kt
+++
@@ -1,8 +0,0 @@
-package com.gerryjuans.template.trending
-
-import com.gerryjuans.template.api.GithubRepo
-
-enum class TrendingSortType(val getSortedItems: (List<GithubRepo>) -> List<GithubRepo>) {
-    NAME    ( { items -> items.sortedBy { it.name } } ),
-    STARS   ( { items -> items.sortedByDescending { it.stars } } )
-}
